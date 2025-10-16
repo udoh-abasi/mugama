74c947397bb147ea703764d5d36fe1bc0cf7b52f
Udoh Abasi <udohabasionosunday@gmail.com>
2025-10-16 12:03:59 +0100

    Added codes for Metadata and Schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,49 @@ export const metadata: Metadata = {
   title: "Mugama Technologies",
   description:
     "A versatile Enterprise Transformation and Technologies Consulting firm",
+  openGraph: {
+    title: "Mugama Technologies",
+    description:
+      "A versatile Enterprise Transformation and Technologies Consulting firm",
+    type: "website",
+    url: "https://mugama.vercel.app",
+  },
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Mugama Technologies",
+  url: "https://mugama.vercel.app",
+  logo: "https://mugama.vercel.app/logo.webp",
+  image: "https://mugama.vercel.app/logo.webp",
+  // email: "",
+  foundingDate: "2020-07-15",
+  keywords: "Mugama Technologies",
+  legalName: "Mugama Limited",
+  description:
+    "Mugama Technologies is a versatile Enterprise Transformation and Technologies Consulting firm",
+  address: {
+    "@type": "PostalAddress",
+    // streetAddress: "Plot 1245, Adetokunbo Ademola Crescent, Wuse II",
+    addressLocality: "Abuja",
+    addressRegion: "FCT",
+    // postalCode: "900288",
+    addressCountry: "NG",
+  },
+  contactPoint: {
+    "@type": "ContactPoint",
+    // telephone: "+234-809-000-0000",
+    contactType: "Customer Service",
+    areaServed: "NG",
+    availableLanguage: ["English"],
+  },
+  companyRegistration: {
+    "@type": "Certification",
+    name: "Certificate of Incorporation",
+    certificationIdentification: "RC 1688059",
+    issuedBy: "Corporate Affairs Commission (CAC)",
+  },
 };
 
 export default function RootLayout({
@@ -48,6 +91,11 @@ export default function RootLayout({
           }}
         />
         <Footer />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </body>
     </html>
   );
